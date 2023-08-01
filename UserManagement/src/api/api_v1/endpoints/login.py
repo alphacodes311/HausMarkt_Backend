8a6e87bf6634ecc88ecf8e297896bedc49230705
Pallavi Debnath <Pallavi.Debnath@sennder.com>
2023-08-01 10:04:45 +0200

    first

--- UserManagement/src/api/api_v1/endpoints/login.py
+++ UserManagement/src/api/api_v1/endpoints/login.py
@@ -1,11 +1,11 @@
-# from UserManagement.app.main import app
+# from UserManagement.src.main import src
 #
 #
-# @app.get("/")
+# @src.get("/")
 # async def root():
 #     return {"message": "Hello World"}
 #
 #
-# @app.get("/hello/{name}")
+# @src.get("/hello/{name}")
 # async def say_hello(name: str):
 #     return {"message": f"Hello {name}"}
